ed57c41c9c31fd04cbb02741b29d50a751f7917a
Jimmy Ha <herereadthis@gmail.com>
2014-09-04 20:55:12 -0400

    initial commit for foundations

---
+++ app/scripts/app.js
@@ -0,0 +1,12 @@
+'use strict';
+
+/**
+ * @ngdoc overview
+ * @name noonvaleApp
+ * @description
+ * # noonvaleApp
+ *
+ * Main module of the application.
+ */
+angular
+  .module('noonvaleApp', []);
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name noonvaleApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the noonvaleApp
+ */
+angular.module('noonvaleApp')
+  .controller('MainCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
