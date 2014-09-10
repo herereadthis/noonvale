fedce4966e8aa82669510ea637e6b572eb804c8c
Jimmy Ha <herereadthis@gmail.com>
2014-09-10 01:24:03 -0400

    can now detect winner

--- app/scripts/controllers/main.js
+++ app/scripts/controllers/main.js
@@ -8,7 +8,7 @@
  * Controller of the noonvaleApp
  */
 angular.module('noonvaleApp')
-    .controller('MainCtrl', ['$scope', 'randomSelect', function ($scope, randomSelect) {
+    .controller('MainCtrl', ['$scope', 'TicTacFinger', function ($scope, TicTacFinger) {
         $scope.ticTac = [
             undefined,undefined,undefined,
             undefined,undefined,undefined,
@@ -17,21 +17,38 @@ angular.module('noonvaleApp')
         $scope.asdf = false;
         $scope.clickSelect = function(index) {
             if ($scope.ticTac[index] !== undefined) {
-                return 'selected    ';
-            }
-        };
-        $scope.markCell = function(index) {
-            var nextMark;
-            if ($scope.ticTac[index] === undefined) {
-                // console.log('markCell',index);
-                $scope.ticTac[index] = 1;
-                // console.log($scope.ticTac.toString());
-                // if there are still values to click
-                if ($scope.ticTac.indexOf(undefined) > -1) {
-                    nextMark = randomSelect.bar($scope.ticTac);
-                    $scope.ticTac[nextMark] = 0;
+                if ($scope.ticTac[index] === 1) {
+                    return 'selected mark_x';
+                }
+                else {
+                    return 'selected mark_o';
                 }
             }
         };
+        $scope.markCell = function(index) {
+            var nextMark, winner;
+            if ($scope.ticTac[index] === undefined) {
+                // console.log('markCell',index);
+                $scope.ticTac[index] = 1;
+                winner = TicTacFinger.whoWon($scope.ticTac);
+                // console.log($scope.ticTac.toString());
+                if (winner === 1) {
+                    console.log('winner is X');
+                }
+                else {
+                    // if there are still values to click
+                    if ($scope.ticTac.indexOf(undefined) > -1) {
+                        nextMark = TicTacFinger.randomSelect($scope.ticTac);
+                        $scope.ticTac[nextMark] = 0;
+                        if (TicTacFinger.whoWon($scope.ticTac) === 0) {
+                            console.log('winner is O');
+                        }
+                    }
+                }
+            }
+        };
+        // $scope.$watchCollection('ticTac', function() {
+        //     console.log($scope.ticTac.toString());
+        // });
     }]);
 
--- app/scripts/services.js
+++ app/scripts/services.js
@@ -2,12 +2,9 @@
 /* Services */
 var ticTacServices = angular.module('ticTacServices', ['ngResource']);
 
-// The factory function is similar to a controller's constructor in that both
-// can declare dependencies to be injected via function arguments.
-// The "Phone" service is declared as a dependency on the $resource service
-ticTacServices.factory('randomSelect', function(){
+ticTacServices.factory('TicTacFinger', function(){
     return {
-        bar: function (ticTacArray) {
+        randomSelect: function (ticTacArray) {
             var randomO, undefinedArray, index;
             undefinedArray = [];
             for (index in ticTacArray) {
@@ -16,9 +13,47 @@ ticTacServices.factory('randomSelect', function(){
                 }
             }
             randomO = undefinedArray[Math.floor(Math.random() * undefinedArray.length)];
-            console.log(randomO);
+            // console.log(randomO);
             return randomO;
 
+        },
+        whoWon: function(ticTacArray) {
+            var row1, row2, row3, col1, col2, col3, dia1, dia2,
+                arrayMatch, winSets, winner, _i, checkSum;
+            arrayMatch = function(arr) {
+                if (arr[0] === arr[1] && arr[0] === arr[2]) {
+                    return true;
+                }
+                else {
+                    return false;
+                }
+            };
+            row1 = [ticTacArray[0], ticTacArray[1], ticTacArray[2]];
+            row2 = [ticTacArray[3], ticTacArray[4], ticTacArray[5]];
+            row3 = [ticTacArray[6], ticTacArray[7], ticTacArray[8]];
+            col1 = [ticTacArray[0], ticTacArray[3], ticTacArray[6]];
+            col2 = [ticTacArray[1], ticTacArray[4], ticTacArray[7]];
+            col3 = [ticTacArray[2], ticTacArray[5], ticTacArray[8]];
+            dia1 = [ticTacArray[0], ticTacArray[4], ticTacArray[8]];
+            dia2 = [ticTacArray[2], ticTacArray[4], ticTacArray[6]];
+            winSets = [row1, row2, row3, col1, col2, col3, dia1, dia2];
+
+            winner = false;
+            _i = 0;
+            while (_i < winSets.length) {
+                checkSum = parseInt(winSets[_i][0],10) +
+                    parseInt(winSets[_i][1],10) +
+                    parseInt(winSets[_i][2],10);
+                if (checkSum >= 0) {
+                    if (arrayMatch(winSets[_i]) === true) {
+                        winner = winSets[_i][0];
+                        break;
+                    }
+                }
+                _i = _i + 1;
+            }
+
+            return winner;
         }
     };
 });
