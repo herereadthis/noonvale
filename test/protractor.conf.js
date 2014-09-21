a21d910b3957cfcd2ea5856e99627fb0dc651879
Jimmy Ha <herereadthis@gmail.com>
2014-09-21 12:11:27 -0400

    typo

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,4 +1,4 @@
-http://stackoverflow.com/questions/19066747/integrating-protractor-for-e2e-testing-with-yeoman-in-grunt-file-for-angular-j
+// http://stackoverflow.com/questions/19066747/integrating-protractor-for-e2e-testing-with-yeoman-in-grunt-file-for-angular-j
 exports.config = {
     // seleniumAddress: 'http://localhost:4444/wd/hub',
     specs: ['e2e/*.js'],
