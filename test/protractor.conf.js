44c533e24e4ebd10c88cc7ce44ba957450c090ee
herereadthis <herereadthis@gmail.com>
2014-09-30 00:15:38 -0400

    #6 update dependencies

--- test/protractor.conf.js
+++ test/protractor.conf.js
@@ -1,6 +1,8 @@
 // http://stackoverflow.com/questions/19066747/integrating-protractor-for-e2e-testing-with-yeoman-in-grunt-file-for-angular-j
 exports.config = {
     // seleniumAddress: 'http://localhost:4444/wd/hub',
-    specs: ['e2e/*.js'],
+    specs: [
+        'e2e/*.js'
+    ],
     baseUrl: 'http://localhost:9001' //default test port with Yeoman
 }
